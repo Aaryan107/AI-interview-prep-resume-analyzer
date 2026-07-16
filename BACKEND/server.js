32d0ed1955cef3d25ed1efad99a0be82ad83dfa3
Aaryan107 <aaryanchauhan247x@gmail.com>
2026-07-16 23:23:21 +0530

    configure app for render deployment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,6 +5,6 @@ const connectToDB=require('./src/config/DataBase')
 
 connectToDB()
 
-app.listen(3000,function(){
-    console.log('listening on port 3000')
+app.listen(process.env.PORT || 3000,function(){
+    console.log(`listening on port ${process.env.PORT || 3000}`)
 })
